22c014f71cd7692d4df2f27f0c687be224b7976b
jarek <jaroslaw.kuklis@smartbear.com>
2023-06-27 14:57:51 +0200

    scenario

---
+++ TestProject1/Scenarios/TestScenario.feature
@@ -0,0 +1,5 @@
+﻿@Reports
+Feature: TestScenario
+
+  Scenario: A description of your business scenario
+    Given The tested app is running
--- TestProject1/Script/Unit5.py
+++ TestProject1/Script/Unit5.py
@@ -1,4 +1,4 @@
-﻿#@calculator
+﻿"""@calculator
 def Test8():
     #Clicks the 'TrayButton' object.
     Aliases.explorer.wndShell_SecondaryTrayWnd.TrayButton.Click(21, 18)
@@ -11,4 +11,20 @@ def Test8():
     #Clicks the 'Five' object.
     Aliases.Microsoft_WindowsCalculator.Calculator.NavView.LandmarkTarget.Number_pad.Five.Click()
     #Closes the 'wndCalculator' window.
+    Aliases.ApplicationFrameHost.wndCalculator.Close()"""
+
+@given("The tested app is running")
+def step_impl():
+    #Clicks the 'TrayButton' object.
+    Aliases.explorer.wndShell_SecondaryTrayWnd.TrayButton.Click(21, 18)
+    #Enters 'ca' in the 'wndSearch' object.
+    Aliases.CortanaUI.wndSearch.Keys("ca")
+    #Enters 'l' in the 'Search_box' object.
+    Aliases.CortanaUI.wndSearch.Search_box.Keys("l")
+    #Clicks the 'panelApp' object.
+    Aliases.CortanaUI.pageBing.panelApp.Click(83, 8)
+    #Clicks the 'Five' object.
+    Aliases.Microsoft_WindowsCalculator.Calculator.NavView.LandmarkTarget.Number_pad.Five.Click()
+    #Closes the 'wndCalculator' window.
     Aliases.ApplicationFrameHost.wndCalculator.Close()
+
