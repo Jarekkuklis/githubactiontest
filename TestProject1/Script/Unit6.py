a9b70df40f5e5c712e980ec33c81bf9a747c285c
jarek <jaroslaw.kuklis@smartbear.com>
2023-12-21 12:56:05 +0100

    logs test

--- TestProject1/Script/Unit6.py
+++ TestProject1/Script/Unit6.py
@@ -1,9 +1,8 @@
-﻿def getTime():
-  BuiltIn.SendMail("jaroslaw.kuklis@smartbear.com", "jaroslaw.kuklis@smartbear.com", "", "jaroslaw.kuklis@smartbear.com", "KeywordTestLog", "Attached are the log results", "C:\\Work\\Log.mht")
-
+﻿def logmsg():
+  Log.Message("msg")
   
-def Test():
-    if SendMail("jaroslaw.kuklis@gmail.com", "gmail.com", "aa", "wworkk1996@gmail.com", "KeywordTestLog", "Attached are the log results", "C:\\Work\\Log.mht"):
-      Log.Message("Mail was sent")
-    else:
-      Log.Warning("Mail was not sent")
+def logwar():
+  Log.Warning("war")
+  
+def logerr():
+  Log.Error("err")
