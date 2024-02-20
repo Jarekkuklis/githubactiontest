66deace25a881bd38ebb088b89e850e5fc3e35f1
jarek <jaroslaw.kuklis@smartbear.com>
2024-02-20 14:20:33 +0100

    test VM agent

--- TestProject1/Script/Unit5.py
+++ TestProject1/Script/Unit5.py
@@ -1,2 +1,3 @@
-﻿
-
+﻿def Sample_Run_Browser():
+  Browsers.Item["chrome"].RunOptions = "--disable-web-security --user-data-dir=C:\\Users\\jaroslaw.kuklis\\AppData\\Local\\Google\\Chrome\\User Data\\Default --disable-site-isolation-trials"
+  Browsers.Item["chrome"].Run()
--- TestProject1/Script/Unit6.py
+++ TestProject1/Script/Unit6.py
@@ -1,11 +1,13 @@
-﻿def logmsg():
-  
-  if eval(ProjectSuite.Variables.test2) == True:
-    Log.Message(ProjectSuite.Variables.test2)
-  else: False
-  
-def logmsg2():
-  
-  
-    Log.Message(ProjectSuite.Variables.test2)
-  
+﻿def Test1():
+    Project.Variables.Var11.Reset()
+    while not Project.Variables.Var11.IsEOF():
+        #Posts an information message to the test log.
+        Log.Message(Project.Variables.Var11.Value["F1"], "")
+        if Project.Variables.Var11.Value["F1"] == 4:
+            #Posts an error to the test log.
+            Log.Error("", "")
+        Project.Variables.Var11.Next()
+
+def EventControl1_OnLogError(Sender, LogParams):
+    Log.Message("the event work")
+    pass
