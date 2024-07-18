1006c9b299ad76aca0806d502320bc603adeb489
jarek <jaroslaw.kuklis@smartbear.com>
2024-07-18 15:09:44 +0200

    delete

--- TestProject1/Script/Unit3.py
+++
@@ -1,3 +0,0 @@
-﻿def CreatingAndWritingToTextFile():
-    aqFile.WriteToTextFile("C:\\Users\\jaroslaw.kuklis\\Downloads\\mlb_players.tsv", "Hello!", aqFile.ctUTF8)
-    Log.Message("The file is created and the specified text is written to it successfully.")
