9eb9a36615ae646e3e13828b0c21dfc28b65cc82
jarek <jaroslaw.kuklis@smartbear.com>
2025-06-11 13:07:22 +0200

    abcds

--- TestProject1/Script/Unit1.js
+++ TestProject1/Script/Unit1.js
@@ -1,4 +1,4 @@
-﻿
-function CloseAllTabs() {
-   var a = Log.Message(a)
-  }
+﻿function Test1()
+{
+  Sys.Browser("chrome").Page("https://www.google.com/search*").Panel("main").Panel("cnt").Panel("rcnt").Click()
+}
