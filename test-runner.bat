45a9a084addaa7844d54f8db0b640d0325ca0784
Jarekkuklis <jaroslaw.kuklis@smartbear.com>
2025-06-16 15:13:24 +0200

    Update test-runner.bat

--- test-runner.bat
+++ test-runner.bat
@@ -1,4 +1,5 @@
 @echo off
-set TEST_EXECUTE_ACCESS_KEY=b12f4231-34da-4ccd-9be8-25b2b17ba825
-set PROJECT="C:\Users\jaroslaw.kuklis\Documents\TestComplete 15 Projects\TestProject1\TestProject1.pjs"
-"C:\Program Files (x86)\SmartBear\TestComplete 15\Bin\TestComplete.exe" /run /project:"%PROJECT%" /accesskey:"%TEST_EXECUTE_ACCESS_KEY%"
+set TEST_EXECUTE_ACCESS_KEY=%1
+set PROJECT=%2
+"C:\Program Files (x86)\SmartBear\TestExecute 15\Bin\TestExecute.exe" /run /project:"%PROJECT%" /accesskey:"%TEST_EXECUTE_ACCESS_KEY%"
+
